df8c4224b2686d736cb4a6b6ef7838f50f915e3c
Max Ziebell <1752114+worldoptimizer@users.noreply.github.com>
2024-01-11 20:37:41 +0100

    v1.0.0

---
+++ HypeDocumentLoad.js
@@ -0,0 +1,23 @@
+/*!
+Hype Document Load 1.0.0
+copyright (c) 2022 Max Ziebell, (https://maxziebell.de). MIT-license
+*/
+/*
+* Version-History
+* 1.0.0 Initial release under MIT-license
+*/
+if("HypeDocumentLoad" in window === false) window['HypeDocumentLoad'] = (function () {
+	
+	function HypeDocumentLoad(hypeDocument, element, event) {
+	   if(hypeDocument.functions().HypeDocumentLoad) {
+		   hypeDocument.functions().HypeDocumentLoad(hypeDocument, element, event);
+	   }
+	}
+	
+	if("HYPE_eventListeners" in window === false) { window.HYPE_eventListeners = Array(); }
+	window.HYPE_eventListeners.push({"type":"HypeDocumentLoad", "callback": HypeDocumentLoad});
+	
+	return {
+		version: '1.0.0',
+	};
+})();
